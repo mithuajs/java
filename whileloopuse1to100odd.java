3d65870d4796f5c6ad7b52b834a7511781a449d8
MD Mobasher Hosen Mithu <234790845+mithuajs@users.noreply.github.com>
2025-11-04 01:57:37 +0600

    Add files via upload

---
+++ whileloopuse1to100odd.java
@@ -0,0 +1,13 @@
+class job {
+
+    public static void main(String[] args) {
+        int i = 1, sum = 0;
+        while (i <= 100) {
+                 sum = sum + i;
+            
+            i++;
+        }
+        System.out.println("The sum  is: " + sum);
+    }
+    
+}
