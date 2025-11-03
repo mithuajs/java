b6656af475e4cd4db52cd2231acd86752e46f0e1
MD Mobasher Hosen Mithu <234790845+mithuajs@users.noreply.github.com>
2025-11-04 01:58:03 +0600

    Add files via upload

---
+++ forloppusein1to100odd.java
@@ -0,0 +1,9 @@
+class job {
+    public static void main(String[] args) {
+        int i, sum = 0;
+        for ( i = 1; i <= 100; i ++) {
+            sum = sum + i;
+        }
+            System.out.println( "the sum is " + sum );
+        }
+    }
