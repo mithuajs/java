dc3cce5cbefe07aaa59cd5dc3a34c9388a9af883
MD Mobasher Hosen Mithu <234790845+mithuajs@users.noreply.github.com>
2025-12-31 02:28:12 +0600

    Add files via upload

---
+++ arraytodataprint.java
@@ -0,0 +1,17 @@
+//array hothe data print korar program:
+public class arraytodataprint {
+
+    public static void main(String[] args) {
+        int arr []= {5,7,10,9,12};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println( arr[i]);
+        }
+    }
+}
+
+
+/* 5
+7
+10
+9
+12 */ 
