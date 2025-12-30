580bf9f743e3b64d868e86ac73417dbc5a9bd588
MD Mobasher Hosen Mithu <234790845+mithuajs@users.noreply.github.com>
2025-12-31 02:32:23 +0600

    Add files via upload

---
+++ tennumbersmalltobigcrome.java
@@ -0,0 +1,11 @@
+//10 ta number ke choto theke boro kore print korar program:
+import java.util.Arrays;
+public class tennumbersmalltobigcrome {
+    public static void main(String[] args) {
+        int arr [] = {5,2,1,8,15,3,10,18,22,25};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
+//[1, 2, 3, 5, 8, 10, 15, 18, 22, 25]
