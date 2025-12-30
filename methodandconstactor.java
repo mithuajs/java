49a266511cc751fc0c54d82e208d2a66a46f3bc6
MD Mobasher Hosen Mithu <234790845+mithuajs@users.noreply.github.com>
2025-12-31 02:30:40 +0600

    Add files via upload

---
+++ methodandconstactor.java
@@ -0,0 +1,16 @@
+//method ar constructor   baboher kor aa program :
+public class methodandconstactor {
+    void main() { //class name ar method name same hole constructor hoy name akoi na hohe constructor hoy na .
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        methodandconstactor obj = new methodandconstactor();
+        obj.main();
+    }
+    }
+    
+    //5050
